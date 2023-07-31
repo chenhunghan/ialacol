1328cfe9ae9522240b65251cb0b1b47c0db3bb2c
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2023-07-31 16:05:04 +0300

    Try again
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- request_body.py
+++ request_body.py
@@ -15,30 +15,30 @@ class CompletionRequestBody(BaseModel):
     prompt: str = Field(
         default="", description="The prompt to generate completions for."
     )
-    max_tokens = Optional[int]
-    temperature = Optional[float]
-    top_p = Optional[float]
-    stop = Optional[List[str] | str]
+    max_tokens: Optional[int]
+    temperature: Optional[float]
+    top_p: Optional[float]
+    stop: Optional[List[str] | str]
     stream: bool = Field()
     model: str = Field()
     # llama.cpp specific parameters
-    top_k = Optional[int]
-    repetition_penalty = Optional[float]
-    last_n_tokens = Optional[int]
-    seed = Optional[int]
-    batch_size = Optional[int]
-    threads = Optional[int]
+    top_k: Optional[int]
+    repetition_penalty: Optional[float]
+    last_n_tokens: Optional[int]
+    seed: Optional[int]
+    batch_size: Optional[int]
+    threads: Optional[int]
 
     # ignored or currently unsupported
-    suffix: Any = Any
-    presence_penalty: Any = Any
-    frequency_penalty: Any = Any
-    echo: Any = Any
-    n: Any = Any
-    logprobs: Any = Any
-    best_of: Any = Any
-    logit_bias: Any = Any
-    user: Any = Any
+    suffix: Any
+    presence_penalty: Any
+    frequency_penalty: Any
+    echo: Any
+    n: Any
+    logprobs: Any
+    best_of: Any
+    logit_bias: Any
+    user: Any
 
     class Config:
         arbitrary_types_allowed = True
@@ -64,26 +64,26 @@ class ChatCompletionRequestBody(BaseModel):
     messages: List[ChatCompletionRequestMessage] = Field(
         default=[], description="A list of messages to generate completions for."
     )
-    max_tokens = Optional[int]
-    temperature = Optional[float]
-    top_p = Optional[float]
-    stop = Optional[List[str] | str]
+    max_tokens: Optional[int]
+    temperature: Optional[float]
+    top_p: Optional[float]
+    stop: Optional[List[str] | str]
     stream: bool = Field()
     model: str = Field()
     # llama.cpp specific parameters
-    top_k = Optional[int]
-    repetition_penalty = Optional[float]
-    last_n_tokens = Optional[int]
-    seed = Optional[int]
-    batch_size = Optional[int]
-    threads = Optional[int]
+    top_k: Optional[int]
+    repetition_penalty: Optional[float]
+    last_n_tokens: Optional[int]
+    seed: Optional[int]
+    batch_size: Optional[int]
+    threads: Optional[int]
 
     # ignored or currently unsupported
-    n: Any = Any
-    logit_bias: Any = Any
-    user: Any = Any
-    presence_penalty: Any = Any
-    frequency_penalty: Any = Any
+    n: Any
+    logit_bias: Any
+    user: Any
+    presence_penalty: Any
+    frequency_penalty: Any
 
     class Config:
         arbitrary_types_allowed = True
